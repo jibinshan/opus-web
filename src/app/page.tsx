b9af35c7a72755f2e51459f1ea8710ae265f5661
jibinshan <jibinshanu23@gmail.com>
2025-02-21 16:47:10 +0530

    added new section trusted partner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/ui/Footer";
 import Automate from "./(section)/Automate";
 import AutomateRes from "./(section)/AutomateRes";
 import Contact from "./(section)/Contact";
@@ -7,12 +8,16 @@ import Review from "./(section)/Review";
 import ReviewRes from "./(section)/ReviewRes";
 import ImageSlider from "./(section)/sample";
 import SmartHome from "./(section)/SmartHome";
+import TrustedPartner from "./(section)/TrustedPartner";
 import WeOffer from "./(section)/WeOffer";
 import Working from "./(section)/Working";
+import Whats from "./(section)/Whats";
+import Built from "./(section)/Built";
 
 export default function Home() {
 	return (
 		<div className="w-full h-full flex flex-col justify-center items-center">
+			<TrustedPartner />
 			<WeOffer />
 			<Working />
 			<SmartHome />
@@ -24,16 +29,19 @@ export default function Home() {
 				<AutomateRes />
 			</div>
 			<Empowering />
+			<Built />
 			<div className="hidden md:block md:w-full">
 				<Review />
 			</div>
 			<div className="md:hidden overflow-x-hidden">
 				<ReviewRes />
 			</div>
+			<Whats />
 			<Contact />
+			<Footer />
 			{/* <ImageSlider /> */}
 			{/*
 			 */}
-		</div>
+		</div >
 	);
 }
